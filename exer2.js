e8c160e59982a3a561ef086710f34272132e8b9d
jimmyxlove <jimmyx8love@gmail.com>
2025-09-04 09:06:05 -0300

    ok

---
+++ exer2.js
@@ -0,0 +1,17 @@
+// lista de igredientes do bolo (array)
+
+const igredientes = [
+    "ovos",
+    "2 xicaras de farinha de trigo",
+    "1 xicara de acucar",
+    "1 xicara de leite",
+    "colher de sopa de fermento em po",
+];
+
+// Titulo
+console.log ("receita de bolo simples");
+console.log ("igredientes");
+
+// Exibindo cada igrediente da lista
+
+
